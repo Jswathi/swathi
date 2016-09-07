27e6e912aff25b9d73458e4bee34b8ee2703cb1a
Jswathi <Jswathi@users.noreply.github.com>
2016-09-07 12:06:46 +0530

    Create WorkingDays.java

---
+++ WorkingDays.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class WorkingDays{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+for(int i=0;i<5;i++)
+{
+  String w[i]=sc.next();
+}
+System.out.println("Enter the day");
+String s1=sc.next();
+if(w.contains(s1))
+{
+   System.out.println(s1+"True");
+}
+else
+{
+   System.out.println(s1+"False");
+
+ }
+ }
+ }
