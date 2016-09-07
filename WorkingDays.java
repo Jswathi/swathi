bdb0f39560c626a19e1a843558cab028a8c0f8d2
Jswathi <Jswathi@users.noreply.github.com>
2016-09-07 12:11:26 +0530

    Update WorkingDays.java

--- WorkingDays.java
+++ WorkingDays.java
@@ -5,7 +5,7 @@ class WorkingDays{
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 String w[]=new String[5];
-for(int i=0;i<5;i++)
+for(int i=0;i<6;i++)
 {
    w[i]=sc.next();
 }
